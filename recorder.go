63d15af30f6d797da0b017ea954aee5c293026dd
Brandon <grigullbrandon@gmail.com>
2018-09-14 19:14:08 -0700

    add recording function

--- recorder.go
+++ recorder.go
@@ -2,34 +2,37 @@ package cloudskine
 
 import (
 	"encoding/csv"
-	"os"
+	"fmt"
 	"log"
+	"os"
 )
 
 type Recorder struct {
 	writer *csv.Writer
 }
 
-func (r *Recorder) setWriter(path string) {
+func (r *Recorder) recordRecord(path string, note Note, tagged bool, separate bool) {
+	var record []string
+	var file *os.File
+
+	if tagged == true {
+		record = note.ToTaggedRecord(separate)
+	} else {
+		record = note.ToRecord()
+	}
+
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		file, err := os.Create(path)
+		file, err = os.Create(path)
 		if err != nil {
-			log.Println(err.Error())
+			log.Panic(fmt.Sprintf("Error: %s", err.Error()))
 		}
-		r.writer = csv.NewWriter(file)
+	}
+
+	r.writer = csv.NewWriter(file)
+	defer file.Close()
+	defer r.writer.Flush()
+
+	if err := r.writer.Write(record); err != nil {
+		log.Panic(fmt.Sprintf("Error: %s", err.Error()))
 	}
 }
-
-func (r *Recorder) writeRecord(path string, note Note) {
-	if r.writer == nil {
-		r.setWriter(path)
-	}
-	defer func() {
-		r.writer.Flush()
-		if err := r.writer.Error(); err != nil {
-			log.Println(err.Error())
-		}
-	}()
-
-	r.writer.Write(note.toRecord())
-}	
