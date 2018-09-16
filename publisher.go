8f69289389ad03584c0bfb065498f7b2889dbea1
Brandon <grigullbrandon@gmail.com>
2018-09-16 11:02:14 -0700

    import s3 packages

--- publisher.go
+++ publisher.go
@@ -1,13 +1,19 @@
 package cloudskine
 
-type Publisher interface {
-	Publish(Note)
+import (
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
+)
+
+type Publisher struct {
+    Publish()
 }
 
 type S3Publisher struct {
-	BucketUrl string
+    BucketUrl string
 }
 
-func (s3p *S3Publisher) Publish(note Note) {
-
+func (s3p *S3Publisher) Publish() {
+    
 }
