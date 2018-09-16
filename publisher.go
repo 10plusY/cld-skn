c005d6e8a2a43e8ac95e948ebcbb14a83ea839c5
Brandon <grigullbrandon@gmail.com>
2018-09-16 12:02:13 -0700

    refactor pub code

--- publisher.go
+++ publisher.go
@@ -1,62 +1,50 @@
 package cloudskine
 
 import (
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 
-    "os"
-    "fmt"
-    "bytes"
-    "net/http"
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 type Publisher interface {
-    Publish() error
+	Publish() error
 }
 
 type S3Publisher struct {
-    BucketUrl string
-    Session *session.Session
-    Logger Logger
+	BucketUrl string
+	Session   *session.Session
+	Logger    Logger
 }
 
 func getPutObject(file *os.File, size int64) (interface{}, error) {
-    buffer := make([]byte, size)
-    file.Read(buffer)
-    return s3.New(s3p.Session).PutObject(&s3.PutObjectInput{
-       Bucket: aws.String(s3p.BucketUrl),
-       Key: aws.String(file.Name()),
-       ACL: aws.String("private"),
-       Body: bytes.NewReader(buffer),
-       ContentLength: aws.Int64(size),
-       ContentType: aws.String(http.DetectContentType(buffer)),
-       ContentDisposition: aws.String("attachment"),
-       ServerSideEncryption: aws.String("AWS256"),
-   })
+	buffer := make([]byte, size)
+	file.Read(buffer)
+	return s3.New(s3p.Session).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(s3p.BucketUrl),
+		Key:                  aws.String(file.Name()),
+		ACL:                  aws.String("private"),
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(size),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AWS256"),
+	})
 
 }
 
 func (s3p *S3Publisher) Publish(file *os.File) error {
-    var info os.FileInfo
-    var size int64
-    var err error
+	defer file.Close()
 
-    defer file.Close()
-
-    if info, err = file.Stat(); err != nil {
-        s3p.Logger.Log((fmt.Sprintf("Error: %s", err)))
-    } else {
-        size = info.Size()
-    }
-
-    buffer := make([]byte, size)
-    file.Read(buffer)
-
-    _, err = getPutObject()
-    return err
-
-
-
-
+	if info, err := file.Stat(); err != nil {
+		s3p.Logger.Log((fmt.Sprintf("Error: %s", err)), true)
+	} else {
+		size = info.Size()
+		_, err := getPutObject(file, info.Size())
+		return err
+	}
 }
