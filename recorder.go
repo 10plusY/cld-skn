5076e5262428fb1fe2f2e8b442bd48a0435d45b6
Brandon <grigullbrandon@gmail.com>
2018-09-16 10:59:52 -0700

    split recorder logic

--- recorder.go
+++ recorder.go
@@ -9,30 +9,67 @@ import (
 
 type Recorder struct {
 	writer *csv.Writer
+	logger *Logger
+	files  []*os.File
 }
 
-func (r *Recorder) recordRecord(path string, note Note, tagged bool, separate bool) {
-	var record []string
-	var file *os.File
-
-	if tagged == true {
-		record = note.ToTaggedRecord(separate)
-	} else {
-		record = note.ToRecord()
+func (r *Recorder) getFileFromPath(path string) *os.File {
+	var rFile *os.File
+	for _, file := range r.files {
+		if path == file.Name() {
+			rFile = file
+			break
+		}
 	}
 
+	return rFile
+}
+
+func (r *Recorder) getNoteRecord(note Note, tagged, separate bool) []string {
+	if tagged == true {
+		return note.ToTaggedRecord(separate)
+	} else {
+		return note.ToRecord()
+	}
+}
+
+func (r *Recorder) addRecordFile(path string) {
+	var file *os.File
+	var err error
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		file, err = os.Create(path)
-		if err != nil {
-			log.Panic(fmt.Sprintf("Error: %s", err.Error()))
-		}
+	} else {
+		file, err = os.Open(path)
+	}
+
+	if err != nil {
+		log.Panic(fmt.Sprintf("Error: %s", err.Error()))
+	}
+
+	r.files = append(r.files, file)
+}
+
+func (r *Recorder) setWriterFile(path string) {
+	file := r.getFileFromPath(path)
+	if file == nil {
+		log.Panic("File does not exist")
+	}
+
+	r.writer = csv.NewWriter(file)
+}
+
+func (r *Recorder) writeNoteRecord(path string, record []string) {
+	file := r.getFileFromPath(path)
+	if file == nil {
+		log.Panic("File does not exist")
 	}
 
 	r.writer = csv.NewWriter(file)
 	defer file.Close()
 	defer r.writer.Flush()
 
-	if err := r.writer.Write(record); err != nil {
-		log.Panic(fmt.Sprintf("Error: %s", err.Error()))
+	err := r.writer.Write(record)
+	if err != nil {
+		log.Panic("Cannot write record")
 	}
 }
